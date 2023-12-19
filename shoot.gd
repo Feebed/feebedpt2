bbbfe06917eab0a9277be0a6109b852d303cf963
max <vwa11233@gmail.com>
2023-12-19 17:00:39 -0500

    tuesday

---
+++ shoot.gd
@@ -0,0 +1,18 @@
+extends Camera3D
+var bullet = preload("res://rigid_body_3d.tscn")
+@onready var shootpostion = $"../../CollisionShape3D/MeshInstance3D/Marker3D"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var root = get_tree().get_root().get_node("world") 
+	if Input.is_action_just_pressed("shoot"):
+		var newbullet = bullet.instantiate() 
+		newbullet.position= shootpostion.global_position
+		newbullet.rotation.y = $"../../CollisionShape3D/MeshInstance3D".rotation.y 
+		root.add_child(newbullet)
+	pass
